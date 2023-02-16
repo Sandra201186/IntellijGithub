b18b726564b988182c947e5a65f8d103c3b2cca3
Sandra201186 <alekbz.az@gmail.com>
2023-02-16 23:08:40 +0100

    add new multiple test

--- src/standalone/LoginFormTest.java
+++ src/standalone/LoginFormTest.java
@@ -4,4 +4,8 @@ public class LoginFormTest {
     public static void main(String[] args) {
 
     System.out.println("Adding new automation test for Loggin form");
+
+    System.out.println("Added new multiple test");
+        System.out.println("Add new color");
+
 }

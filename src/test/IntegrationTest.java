6f3c89113b31135db26182de092953a52e152e5c
Sandra201186 <alekbz.az@gmail.com>
2023-02-16 21:32:46 +0100

    my first commit

---
+++ src/test/IntegrationTest.java
@@ -0,0 +1,25 @@
+package test;
+
+public class IntegrationTest {
+
+    public static void main(String[] args) {
+        //this test will be run one time
+
+
+
+        System.out.println("Hello World!!");
+        sayHello("Sandra");
+
+        //todo:Add multiple users
+
+    }
+    private int age = 15;
+
+    public IntegrationTest(int age) {
+        this.age = age;
+    }
+
+    public static void sayHello(String name) {
+        System.out.println("Hello "+ name);
+    }
+}
